f3cc6e1d69764abfe3d0fb25e604522df41d595f
7estelle <layestelle7@gmail.com>
2021-10-06 17:45:20 +0200

    update

--- script.js
+++ script.js
@@ -2,25 +2,53 @@ let svg = d3.select("svg");
 
 svg.style("background-color", "black");
 
-let coordonnees=[
-    {x:25,y:30},
-    {x:55,y:60},
-    {x:95,y:20},
-    {x:5,y:70},
-    {x:80,y:95}
-];
+// Panneau d'indication en haut
+d3.select("section")
+    .append("p")
+    .text("Nombre de vies restantes : ");
 
-svg
-    .selectAll("path")
-    .data(coordonnees)
-    .enter()
-    .append("path")
-    .attr("d", "M0,0 L-10,0 M0,0 L8.1,5.9 M0,0 L8.1,-5.9 M0,0 L-3.1,9.5 M0,0 L-3.1,-9.5")
+d3.select("section")
+    .append("p")
+    .text("Score : ");
 
-// alternative avec transform :
-    .attr("x", 0) 
-    .attr("y", 0) 
-    .attr("transform", d=>`translate(${d.x}, ${d.y})`)
 
-    .style("stroke","yellow");
+// Zone du joueur
+d3.select("svg")
+.append("rect")
+.attr("x", 0) 
+.attr("y", 85)
+.attr("width", "100")
+.attr("height", "15")
+.attr("fill", "red");
 
+//on ajoute un groupe qui sert de "couche principale"
+// svg.select("rect")
+// .append("g");
+
+// Avatar du joueur
+svg.select("rect")
+.append("circle")
+.attr("cx",10)
+.attr("cy",10)
+.attr("r",50)
+.attr("fill","blue");
+
+
+//puis un fantome, qui sera toujours au premier plan (initialement invisible)
+// svg.append("use")
+//     .attr("id","fantome")
+//     .attr("href", "#spirale")
+//     .style("display","none")
+//     .style("z-index",2)
+//     .style("opacity",".5");
+
+
+// svg.on("mouseenter", function(e) {    
+//         positionFantome(e);
+//         d3.select("#fantome")
+//             .style("display",null)});
+
+// svg.on("mouseleave", function(e) {
+//                 d3.select("#fantome")
+//                     .style("display","none")
+//             } );
