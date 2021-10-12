fd654567de04213dc234fe7cad12a5a5b96592ec
7estelle <layestelle7@gmail.com>
2021-10-12 17:50:57 +0200

    estelle

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
 let svg = d3.select("svg");
+<<<<<<< Updated upstream
 
 
+=======
+let viesRestantes = 3;
+>>>>>>> Stashed changes
 svg.style("background-color", "black");
 
 // Panneau d'indication en haut
 d3.select("section")
     .append("p")
-    .text("Nombre de vies restantes : ");
+    .text(`Nombre de vies restantes : ${viesRestantes}`);
 
 d3.select("section")
     .append("p")
     .text("Score : ");
 
 
-// Zone du joueur
+// Zone du joueur (rouge)
 d3.select("svg")
     .append("rect")
     .attr("x", 0)
@@ -24,6 +28,7 @@ d3.select("svg")
 
 
 // Avatar du joueur
+<<<<<<< Updated upstream
 svg.append("circle")
     .attr("id", "joueur")
     .attr("cx", 50)
@@ -60,3 +65,92 @@ svg.on("mousemove", function (e) {
 
 
 
+=======
+svg.select("rect")
+.append("circle")
+.attr("cx",10)
+.attr("cy",10)
+.attr("r",50)
+.attr("fill","blue");
+
+
+// puis un fantome, qui sera toujours au premier plan (initialement invisible)
+// svg.append("use")
+//     .attr("id","fantome")
+//     .attr("href", "#spirale")
+//     .style("display","none")
+//     .style("z-index",2)
+//     .style("opacity",".5");
+
+
+// svg.on("mouseenter", function(e) {    
+//         positionFantome(e);
+//         d3.select("#fantome")
+//             .style("display",null)});
+
+// svg.on("mouseleave", function(e) {
+//                 d3.select("#fantome")
+//                     .style("display","none")
+//             } );
+
+
+
+// ENNEMIS (q7)
+let positionEnnemis=[
+    {x:entierAlea(100),y:0, vy:1},
+    {x:entierAlea(100),y:0, vy:2},
+    {x:entierAlea(100),y:0, vy:3}
+];
+
+function entierAlea(n) {
+    return Math.floor(Math.random()*n);
+}
+function vitesseAlea(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min +1)) + min;
+  }
+
+function creationSuppressionEnnemis(){
+    let lien = svg.selectAll(".ennemi")
+        .data(positionEnnemis);
+    lien.enter()
+        .append("use")
+        .attr("class", "ennemi")
+        .attr("href", "#def_ennemi");
+    lien.exit()
+        .remove();    
+    placeEnnemis();
+}
+function placeEnnemis() {
+    svg.selectAll(".ennemi") 
+        .attr("transform", d=>`translate(${d.x},${d.y})`);
+}
+function mouvementEnnemis() {
+    positionEnnemis.forEach(d=>{        
+        //chaque ennemi se déplace de sa vitesse en y
+        d.y+=d.vy;
+    })
+    //les coordonnées ont été modifiées, on fait la mise à jour
+    placeEnnemis();
+}
+creationSuppressionEnnemis();
+setInterval(mouvementEnnemis, 100);
+
+//toutes les 2000ms: un nouvel ennemi est ajouté
+setInterval(function(){
+    positionEnnemis.push({x:entierAlea(100),y:0, vy:vitesseAlea(1,3)});
+    creationSuppressionEnnemis();
+}, 2000);
+
+// Si un ennemi touche le bord opposé, le joueur perd une vie (q9)
+
+compteVies();
+function compteVies() {
+    positionEnnemis.forEach(d=>{        
+    if(d.y = 85){
+        viesRestantes = viesRestantes-1;
+    }
+})
+}
+>>>>>>> Stashed changes
