3d5469ea45e97a7b3958e9b0864940d1cc8e6bd9
Benjamin <larochebenjamin94300@gmail.com>
2021-10-12 17:09:03 +0200

    déplacement
    on peut se déplacer dans le rectangle rouge

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let svg = d3.select("svg");
 
+
 svg.style("background-color", "black");
 
 // Panneau d'indication en haut
@@ -14,33 +15,72 @@ d3.select("section")
 
 // Zone du joueur
 d3.select("svg")
-.append("rect")
-.attr("x", 0) 
-.attr("y", 85)
-.attr("width", "100")
-.attr("height", "15")
-.attr("fill", "red");
+    .append("rect")
+    .attr("x", 0)
+    .attr("y", 85)
+    .attr("width", "100")
+    .attr("height", "15")
+    .attr("fill", "red");
 
-//on ajoute un groupe qui sert de "couche principale"
-// svg.select("rect")
-// .append("g");
 
 // Avatar du joueur
-svg.select("rect")
-.append("circle")
-.attr("cx",10)
-.attr("cy",10)
-.attr("r",50)
-.attr("fill","blue");
+svg.append("circle")
+    .attr("id", "joueur")
+    .attr("cx", 50)
+    .attr("cy", 90)
+    .attr("r", 5)
+    .attr("fill", "blue");
+
+
+let rect = d3.select("rect");
+
+function positionJoueur(e) {
+
+
+    let pointer = d3.pointer(e);
+
+    if (pointer[1] < 85) {
+        d3.select("#joueur")
+            .attr("cx", pointer[0])
+            .attr("cy", "85");
+
+    }
+    // if (pointer[1] > 100) {
+    //     d3.select("#joueur")
+    //         .attr("cx", pointer[0])
+    //         .attr("cy", "100");
+    // }
+    
+    else {
+        d3.select("#joueur")
+            .attr("cx", pointer[0])
+            .attr("cy", pointer[1])
+    }
+
+
+
+}
+
+
+//dÃ©placement
+svg.on("mousemove", function (e) {
+    positionJoueur(e);
+})
+
+
+
+
+
+
+// svg.append("circle")
+// .attr("cx",10)
+// .attr("cy",10)
+// .attr("r",5)
+// .attr("fill","blue");
 
 
 //puis un fantome, qui sera toujours au premier plan (initialement invisible)
-// svg.append("use")
-//     .attr("id","fantome")
-//     .attr("href", "#spirale")
-//     .style("display","none")
-//     .style("z-index",2)
-//     .style("opacity",".5");
+
 
 
 // svg.on("mouseenter", function(e) {    
